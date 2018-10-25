f370d1f2448823509656cef5c928f7742e8b99a5
dselca <dselca@gmail.com>
2018-10-25 14:10:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ function takeANumber(array, name) {
 }
 
 function nowServing(array) {
+  var name = array[0];
   if(array.length === 0) {
     return "There is nobody waiting to be served!";
   } else {
+    array.shift(name);
+    return `Currently serving ${name}.`;
+  }
+}
+
+function currentLine(array) {
+  var nameString = "The line is currently: ";
+  if(array.length === 0) {
+    return "The line is currently empty.";
+  } else {
     for(var i = 0; i < array.length; i++) {
-      return `Currently serving ${array[i]}.`;
+      nameString += `${[i + 1]}. ${array[i]},`;
     }
+  return nameString;
   }
 }
